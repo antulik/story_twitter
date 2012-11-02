6daf649587b9c0b28d56e07ebd9ccbef35821fac
Anton Katunin <antulik@gmail.com>
2012-11-02 18:39:05 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyncTwitter::Application.config.secret_token = '221f7803414eddb776b39a1919cb408b9e105cb11d0c6591fd423eba9896c63d9eef13c070dfa19680a8f286fba3e0277cc94d2ea6901a5016efde1f90c98229'
