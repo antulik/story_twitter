31ffd1a0933366e53cc65c72b5ce8adb39aab3d8
Anton Katunin <antulik@gmail.com>
2012-11-30 16:15:27 +1000

    Sync on twitter login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,24 @@ class SessionsController < ApplicationController
     if params[:provider] == 'story'
       @user = User.where(:username => auth_hash.uid.to_s).first_or_initialize
 
-      @user.story_token = auth_hash['credentials']['token']
+      @user.story_token   = auth_hash['credentials']['token']
       @user.story_user_id = auth_hash['uid']
 
       @user.save!
 
       auto_login(@user)
 
+
     elsif params[:provider] == 'twitter'
       if logged_in?
         current_user.twitter_token  = auth_hash['credentials']['token']
         current_user.twitter_secret = auth_hash['credentials']['secret']
         current_user.save!
+      end
+
+      begin
+        @user.sync_twitter_home_timeline
+      rescue Exception
       end
 
     end
