2de6f30b62241cfb01e14d711798fc99f6c77e3b
Anton Katunin <antulik@gmail.com>
2013-01-12 18:44:31 +1000

    remapped fields

--- app/models/twitter_synchronizer.rb
+++ app/models/twitter_synchronizer.rb
@@ -12,11 +12,11 @@ class TwitterSynchronizer
 
     if calendar.nil?
       story_client.calendar_create({
-          summary:           "Twitter home timeline",
-          synchronizer_name: self.class.name,
-          external_type:     'home_timeline',
-          external_id:       '',
-          color:             "#33ccff"
+          summary:       "Twitter home timeline",
+          external_type: 'home_timeline',
+          external_id:   '',
+          color:         "#33ccff",
+          event_type:    'message',
       })
     else
       calendar
@@ -39,14 +39,36 @@ class TwitterSynchronizer
 
   def sync_home_timeline calendar
     tweets = home_timeline.map do |tweet|
+
+      hs = if tweet.retweeted
+        retweeted_status = tweet.retweeted_status
+        {
+            :author_name         => retweeted_status.user.name,
+            :author_username     => retweeted_status.user.screen_name,
+            :author_thumbnail    => retweeted_status.user.profile_image_url,
+            :description         => retweeted_status.text,
+
+            :posted_by_name      => tweet.user.name,
+            :posted_by_username  => tweet.user.screen_name,
+            :posted_by_thumbnail => tweet.user.profile_image_url,
+            :posted_with_message => tweet.text,
+        }
+      else
+        {
+            :author_name      => tweet.user.name,
+            :author_username  => tweet.user.screen_name,
+            :author_thumbnail => tweet.user.profile_image_url,
+            :description      => tweet.text,
+        }
+      end
+
       {
-          :external_id => tweet.id.to_s,
-          :summary     => tweet.text,
-          :description => "",
+          :external_id => tweet.id_str,
           :size        => tweet.retweet_count,
           :start       => tweet.created_at,
+
           :raw_data    => tweet.to_hash
-      }
+      }.merge(hs)
     end
 
     story_client.events_import(calendar.id, tweets)
